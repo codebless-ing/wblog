c1a33742b595f608945efc998ea2555eddd2c234
Mts <notcolondee@gmail.com>
2024-07-28 10:44:56 -0300

    Add connection to the database and error handling middlewares

--- src/app.js
+++ src/app.js
@@ -1,8 +1,19 @@
 import "dotenv/config";
 import express from "express";
 
+import { errorHandler, xhrErrorHandler, logErrors } from "@middlewares/errorHandler.js";
 import routes from '@routes/index.js';
+import { connectToDatabase } from "@models/index.js";
 
+/* Model */
+await connectToDatabase(
+  process.env.DB_URI,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  process.env.DB_DATABASE
+)
+
+/* HTTP */
 const app = express();
 const port = process.env.PORT || 6010;
 
@@ -12,10 +23,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/', routes);
 
 /** Error handling */
-app.use(() => {
-  throw new Error("Not found");
+app.use((req, res, next) => {
+  const error = new Error('Not found');
+  error.statusCode = 404;
+  next(error);
 });
 
-app.listen(port, () => {
+app.use(logErrors)
+app.use(xhrErrorHandler)
+app.use(errorHandler)
+
+// Spin up the HTTP server
+app.listen(port, async () => {
   console.log(`App listening on port ${port}`);
 })

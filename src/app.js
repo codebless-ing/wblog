892f8491fa52eff1d86a646ad888bc34c6cb3850
Mts <notcolondee@gmail.com>
2024-07-29 20:31:39 -0300

    Move the 404 logic to a middleware

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 import express from "express";
+import "express-async-errors";
 
 import config from "./config.js";
 import { connectToDatabase } from "@models/index.js";
-import { errorHandler, xhrErrorHandler, logErrors } from "@middlewares/errorHandler.js";
+import { errorHandler, xhrErrorHandler, logErrors, notFoundHandler } from "@middlewares/errorHandler.js";
 import { consoleLogger } from "@common/utils/logger.js";
 
 import routes from '@routes/index.js';
@@ -24,12 +25,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/', routes);
 
 /** Error handling */
-app.use((req, res, next) => {
-  const error = new Error('Not found');
-  error.statusCode = 404;
-  next(error);
-});
-
+app.use(notFoundHandler);
 app.use(logErrors)
 app.use(xhrErrorHandler)
 app.use(errorHandler)
@@ -38,3 +34,5 @@ app.use(errorHandler)
 app.listen(config.app.port, async () => {
   consoleLogger.info(`App listening on port ${config.app.port}`);
 })
+
+export default app;

6915da6a6f5ac9f51e6eed8266aed602145b3f4e
Mts <notcolondee@gmail.com>
2024-07-29 20:41:52 -0300

    Improve error handling middleware

--- src/common/middlewares/errorHandler.js
+++ src/common/middlewares/errorHandler.js
@@ -1,9 +1,22 @@
-function logErrors (err, req, res, next) {
-  console.error(err.stack)
+import { newLogger } from "@common/utils/logger.js";
+import { HttpException } from "@common/exceptions/appExceptions.js";
+
+const logger = newLogger("Middleware|errorHandler")
+
+function logErrors(err, req, res, next) {
+  if (!(err instanceof HttpException) || err.statusCode != 404) {
+    logger.error(err.stack)
+  }
+
   next(err)
 }
 
-function xhrErrorHandler (err, req, res, next) {
+function notFoundHandler(req, res, next) {
+  const error = new HttpException(404, "Not found!");
+  next(error);
+}
+
+function xhrErrorHandler(err, req, res, next) {
   if (req.xhr) {
     res.status(500).send({ error: 'Something failed!' })
   } else {
@@ -11,9 +24,9 @@ function xhrErrorHandler (err, req, res, next) {
   }
 }
 
-function errorHandler (err, req, res, next) {
-  res.status(500)
-  res.send({ error: String(err) })
+function errorHandler(err, req, res, next) {
+  res.status(err.statusCode ?? 500)
+  res.send(err.httpMessage ?? "Internal Server Error")
 }
 
-export { logErrors, xhrErrorHandler, errorHandler }
+export { logErrors, xhrErrorHandler, errorHandler, notFoundHandler }

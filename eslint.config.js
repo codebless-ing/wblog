822852857420e201ad410d0cd8e88194588a3ab8
Mts <notcolondee@gmail.com>
2024-07-31 00:25:13 -0300

    Add 'no console' rule to the linter

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,13 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 export default [
-  {ignores: ["**/*.spec.js", "coverage/*", "**/*.mock.js"]},
-  {languageOptions: { globals: { ...globals.browser, ...globals.node  }}},
-  pluginJs.configs.recommended,
+    { ignores: ["**/*.spec.js", "coverage/*", "**/*.mock.js"] },
+    { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+    pluginJs.configs.recommended,
+    {
+        rules: {
+            "no-console": "warn"
+        },
+    },
 ];

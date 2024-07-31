12139ac91f6febaa34e17b84416956cb3c6f2b7e
Mts <notcolondee@gmail.com>
2024-07-31 00:29:22 -0300

    Refactor the router test file so it makes use of the new ModelMock functionalities

--- src/routes/article.spec.js
+++ src/routes/article.spec.js
@@ -1,28 +1,30 @@
-// Stub for instantiated model
-class ModelObjStub {
-    save() {}
-}
-
-/*
- *  IMPORTS
- *  See: https://jestjs.io/docs/ecmascript-modules#module-mocking-in-esm
- */
-jest.unstable_mockModule('@models/article.model.js', () => ({
-    default: ModelObjStub
-}));
-
-const ArticleModel = (await import("@models/article.model.js")).default;
 const request = (await import("supertest")).default;
 const express = (await import("express")).default;
+
+const ModelMock = (await import("@models/model.mock.js")).default;
 const ArticleRoute = (await import("@routes/article.js")).default;
 
 const app = new express();
 
+/*
+ *  HTTP SERVER MOCK
+ */
+app.set('view engine', 'pug');
+app.set('views', './src/resources/views');
+app.engine('pug', (path, options, callback) => {
+    const details = Object.assign({ path, }, options);
+    callback(null, JSON.stringify(details));
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/article', ArticleRoute);
 
 describe('Article Controller', () => {
+    beforeEach(() => {
+        ModelMock.clearModelObject()
+    });
+
+    // CREATE
     describe('when creating an article', () => {
         describe('receiving appropriate data', () => {
             test('should return a page with 200', done => {
@@ -41,7 +43,7 @@ describe('Article Controller', () => {
         })
 
         describe.skip('when receiving no data', () => {
-            test('should return a page with 400', done => { // No view engine yet
+            test('should return a page with 400', done => { // TODO: Need an http sv mock making 404 pages
                 const res = request(app).post('/article')
 
                 res.expect("Content-Type", /html/)
@@ -50,12 +52,43 @@ describe('Article Controller', () => {
         })
 
         describe.skip('when receiving invalid data', () => {
-            test('should return a page with 400', done => { // No view engine yet
+            test('should return a page with 400', done => { // TODO: Need an http sv mock making 404 pages
                 const res = request(app).post('/article')
                 .send({ title: "h" })
 
                 res.expect("Content-Type", /html/)
                 .expect(400, done)
+            })
+        })
+    })
+
+    // READ
+    describe('when getting an article', () => {
+        describe('receiving a valid id', () => {
+            ModelMock.addDocToCollection("SuperCoolIdColonCapitalDee", {
+                title: "Super interesting article (:",
+                body: "Super-duper uber computer ruler o' Vancouver",
+                tags: ["tag", "you're it!"],
+            })
+
+            test('should return a page with 200', done => {
+                const res = request(app)
+                .get('/article/SuperCoolIdColonCapitalDee')
+                .send()
+
+                res.expect("content-type", /html/)
+                .expect(200, done)
+            })
+        })
+
+        describe.skip('when receiving an invalid id', () => {
+            test('should return a page with 404', done => { // TODO: Need an http sv mock making 404 pages
+                const res = request(app)
+                .get('/article/NotSoCoolId')
+                .send()
+
+                res.expect("Content-Type", /html/)
+                .expect(404, done)
             })
         })
     })

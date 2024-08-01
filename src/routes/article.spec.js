29274a798f16d4b09255af8a1f2bc29711cd3cdd
Mts <notcolondee@gmail.com>
2024-07-31 23:30:55 -0300

    Add tests for 'upgrade article' (router)

--- src/routes/article.spec.js
+++ src/routes/article.spec.js
@@ -9,87 +9,107 @@ const app = new express();
 /*
  *  HTTP SERVER MOCK
  */
-app.set('view engine', 'pug');
-app.set('views', './src/resources/views');
-app.engine('pug', (path, options, callback) => {
-    const details = Object.assign({ path, }, options);
+app.set("view engine", "pug");
+app.set("views", "./src/resources/views");
+app.engine("pug", (path, options, callback) => {
+    const details = Object.assign({ path }, options);
     callback(null, JSON.stringify(details));
 });
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/article', ArticleRoute);
+app.use("/article", ArticleRoute);
 
-describe('Article Controller', () => {
-    beforeEach(() => {
-        ModelMock.clearModelObject()
-    });
+afterEach(() => {
+    ModelMock.clearModelObject().clearCollection();
+});
 
+describe("Article Controller", () => {
     // CREATE
-    describe('when creating an article', () => {
-        describe('receiving appropriate data', () => {
-            test('should return a page with 200', done => {
-                const res = request(app)
-                .post('/article')
-                .type("form")
-                .send({
+    describe("when creating an article", () => {
+        describe("receiving appropriate data", () => {
+            test("should return a page with 200", (done) => {
+                const res = request(app).post("/article").type("form").send({
                     title: "Titley title",
                     body: "Post body",
-                    tags: "asdf; qwert; zxcv"
-                })
+                    tags: "asdf; qwert; zxcv",
+                });
 
-                res.expect("content-type", /html/)
-                .expect(200, done)
-            })
-        })
+                res.expect("content-type", /html/).expect(200, done);
+            });
+        });
 
-        describe.skip('when receiving no data', () => {
-            test('should return a page with 400', done => { // TODO: Need an http sv mock making 404 pages
-                const res = request(app).post('/article')
+        describe.skip("when receiving no data", () => {
+            test("should return a page with 400", (done) => {
+                // TODO: Need an http sv mock making 404 pages
+                const res = request(app).post("/article");
 
-                res.expect("Content-Type", /html/)
-                .expect(400, done)
-            })
-        })
+                res.expect("Content-Type", /html/).expect(400, done);
+            });
+        });
 
-        describe.skip('when receiving invalid data', () => {
-            test('should return a page with 400', done => { // TODO: Need an http sv mock making 404 pages
-                const res = request(app).post('/article')
-                .send({ title: "h" })
+        describe.skip("when receiving invalid data", () => {
+            test("should return a page with 400", (done) => {
+                // TODO: Need an http sv mock making 404 pages
+                const res = request(app).post("/article").send({ title: "h" });
 
-                res.expect("Content-Type", /html/)
-                .expect(400, done)
-            })
-        })
-    })
+                res.expect("Content-Type", /html/).expect(400, done);
+            });
+        });
+    });
 
     // READ
-    describe('when getting an article', () => {
-        describe('receiving a valid id', () => {
-            ModelMock.addDocToCollection("SuperCoolIdColonCapitalDee", {
-                title: "Super interesting article (:",
-                body: "Super-duper uber computer ruler o' Vancouver",
-                tags: ["tag", "you're it!"],
-            })
+    describe("when getting an article", () => {
+        describe("receiving a valid id", () => {
+            beforeEach(() => {
+                ModelMock.addDocToCollection("SuperCoolIdColonCapitalDee", {
+                    title: "Super interesting article (:",
+                    body: "Super-duper uber computer ruler o' Vancouver",
+                    tags: ["tag", "you're it!"],
+                });
+            });
 
-            test('should return a page with 200', done => {
-                const res = request(app)
-                .get('/article/SuperCoolIdColonCapitalDee')
-                .send()
+            test("should return a page with 200", (done) => {
+                const res = request(app).get("/article/SuperCoolIdColonCapitalDee").send();
 
-                res.expect("content-type", /html/)
-                .expect(200, done)
-            })
-        })
+                res.expect("content-type", /html/).expect(200, done);
+            });
+        });
 
-        describe.skip('when receiving an invalid id', () => {
-            test('should return a page with 404', done => { // TODO: Need an http sv mock making 404 pages
-                const res = request(app)
-                .get('/article/NotSoCoolId')
-                .send()
+        describe.skip("when receiving an invalid id", () => {
+            test("should return a page with 404", (done) => {
+                // TODO: Need an http sv mock making 404 pages
+                const res = request(app).get("/article/NotSoCoolId").send();
 
-                res.expect("Content-Type", /html/)
-                .expect(404, done)
-            })
-        })
-    })
-})
+                res.expect("Content-Type", /html/).expect(404, done);
+            });
+        });
+    });
+
+    // UPGRADE
+    describe("when updating an article", () => {
+        describe("receiving a valid id", () => {
+            beforeEach(() => {
+                ModelMock.addDocToCollection("6010", {
+                    title: "How to exist",
+                    body: "Step 1: Be born. \n There you are, you're done!",
+                    tags: ["tutorial"],
+                });
+            });
+
+            test("should return a page with 200", (done) => {
+                const res = request(app).put("/article/6010").send();
+
+                res.expect("content-type", /html/).expect(200, done);
+            });
+        });
+
+        describe.skip("when receiving an invalid id", () => {
+            test("should return a page with 404", (done) => {
+                // TODO: Need an http sv mock making 404 pages
+                const res = request(app).put("/article/0709").send();
+
+                res.expect("Content-Type", /html/).expect(404, done);
+            });
+        });
+    });
+});

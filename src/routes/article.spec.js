54a90436d5d6621247ec357093395a7369f9dac8
Mts <notcolondee@gmail.com>
2024-08-01 00:08:19 -0300

    Add a test for receiving invalid data when updating an article

--- src/routes/article.spec.js
+++ src/routes/article.spec.js
@@ -87,26 +87,46 @@ describe("Article Controller", () => {
 
     // UPGRADE
     describe("when updating an article", () => {
-        describe("receiving a valid id", () => {
-            beforeEach(() => {
-                ModelMock.addDocToCollection("6010", {
-                    title: "How to exist",
-                    body: "Step 1: Be born. \n There you are, you're done!",
-                    tags: ["tutorial"],
-                });
+        beforeEach(() => {
+            ModelMock.addDocToCollection("6010", {
+                title: "How to exist",
+                body: "Step 1: Be born. \n There you are, you're done!",
+                tags: ["tutorial"],
             });
+        });
 
+        describe("receiving a valid id", () => {
             test("should return a page with 200", (done) => {
-                const res = request(app).put("/article/6010").send();
+                const res = request(app).put("/article/6010").type("form").send({
+                    title: "New title",
+                    body: "New body",
+                    tags: "New tag",
+                });
 
                 res.expect("content-type", /html/).expect(200, done);
             });
         });
 
-        describe.skip("when receiving an invalid id", () => {
-            test("should return a page with 404", (done) => {
+        describe("receiving invalid data", () => {
+            test.skip("should return a page with 400", (done) => {
+                // TODO: This one depends on a view being returned with 400
+                const res = request(app).put("/article/6010").type("form").send({
+                    title: "",
+                    tags: 1,
+                });
+
+                res.expect("Content-Type", /html/).expect(400, done);
+            });
+        });
+
+        describe("receiving an invalid id", () => {
+            test.skip("should return a page with 404", (done) => {
                 // TODO: Need an http sv mock making 404 pages
-                const res = request(app).put("/article/0709").send();
+                const res = request(app).put("/article/0709").type("form").send({
+                    title: "My experiences with marketing my game",
+                    body: "Basically, I just put 'featuring AsbelianKeys' on the cover and we were sold out of physical (and digital) medias in 9 hours.",
+                    tags: "gamedev; marketing",
+                });
 
                 res.expect("Content-Type", /html/).expect(404, done);
             });

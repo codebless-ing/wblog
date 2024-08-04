2122a8dc1f90e4786d4ba7fa4c1977ab389a687a
Mts <notcolondee@gmail.com>
2024-08-03 21:53:24 -0300

    Update article controller so it makes use of input dtos

--- src/http/article.controller.spec.js
+++ src/http/article.controller.spec.js
@@ -1,6 +1,12 @@
+import { CreateArticleInputDto } from "@common/dto/article/create.dto.js";
+import { ReadArticleInputDto } from "@common/dto/article/read.dto.js";
+import { UpdateArticleInputDto } from "@common/dto/article/update.dto.js";
+import { DeleteArticleInputDto } from "@common/dto/article/delete.dto.js";
 import { HttpException } from "@common/exceptions/appExceptions.js";
+
 const ModelMock = (await import("@models/model.mock.js")).default;
 const controller = new (await import("@controllers/article.controller.js")).default();
+const service = (await import("@services/article.js")).default;
 
 /*
  * RESPONSE STUB
@@ -11,10 +17,26 @@ const res = {
     render: jest.fn(() => {}),
 };
 
+/*
+ * SERVICE SPIES
+ */
+const spies = {
+    create: jest.spyOn(service, "create"),
+    read: jest.spyOn(service, "read"),
+    update: jest.spyOn(service, "update"),
+    delete: jest.spyOn(service, "delete"),
+};
+
 afterEach(() => {
     res.status.mockClear();
     res.render.mockClear();
     ModelMock.clearModelObject().clearCollection();
+
+    for (const k in spies) {
+        if (Object.prototype.hasOwnProperty.call(spies, k)) {
+            spies[k].mockClear();
+        }
+    }
 });
 
 describe("Article controller", () => {
@@ -28,12 +50,17 @@ describe("Article controller", () => {
                     body: {
                         title: "Titley title",
                         body: "Post body",
-                        tags: "asdf; qwert; zxcv",
+                        tags: ["asdf", "qwert", "zxcv"],
                     },
                 };
             });
 
             test.skip("should return a view", () => {});
+
+            test("should communicate with service layer through the use of dto", async () => {
+                await controller.create(req, res);
+                expect(spies.create).toBeCalledWith(expect.any(CreateArticleInputDto));
+            });
 
             test("should respond with 200", async () => {
                 await controller.create(req, res);
@@ -44,10 +71,16 @@ describe("Article controller", () => {
         });
 
         describe("with invalid or NO data", () => {
-            test.skip("should return a view", () => {});
+            let empty = { body: {} };
+            let invalid = { body: {} };
 
-            test("should throw a 404 HTTPException", () => {
-                expect(controller.create({}, res)).rejects.toThrow(HttpException);
+            beforeEach(() => {
+                invalid.body = { title: 3, tags: "" };
+            });
+
+            test("should throw a 400 HTTPException", () => {
+                expect(controller.create(invalid, res)).rejects.toThrow(HttpException);
+                expect(controller.create(empty, res)).rejects.toThrow(HttpException);
             });
 
             // TODO Create more tests when validation, view engine and contracts are implemented
@@ -62,6 +95,13 @@ describe("Article controller", () => {
                     id: "66a941da61910f79bb7e22c7",
                 },
             };
+
+            test("should communicate with service layer through the use of dto", async () => {
+                ModelMock.addDocToCollection("66a941da61910f79bb7e22c7", {});
+
+                await controller.read(req, res);
+                expect(spies.read).toBeCalledWith(expect.any(ReadArticleInputDto));
+            });
 
             test("should respond with 200", async () => {
                 ModelMock.addDocToCollection("66a941da61910f79bb7e22c7", {});
@@ -97,10 +137,15 @@ describe("Article controller", () => {
                     body: {
                         title: "Big titley",
                         body: "Bodey",
-                        tags: "uiop; jklç",
+                        tags: ["uiop", "jklc"]
                     },
                 };
                 ModelMock.addDocToCollection(req.params.id, { title: "a", body: "b" });
+            });
+
+            test("should communicate with service layer through the use of dto", async () => {
+                await controller.update(req, res);
+                expect(spies.update).toBeCalledWith(expect.any(UpdateArticleInputDto));
             });
 
             test("should respond with 200", async () => {
@@ -135,6 +180,11 @@ describe("Article controller", () => {
                     },
                 };
                 ModelMock.addDocToCollection(req.params.id, {});
+            });
+
+            test("should communicate with service layer through the use of dto", async () => {
+                await controller.delete(req, res);
+                expect(spies.delete).toBeCalledWith(expect.any(DeleteArticleInputDto));
             });
 
             test("should respond with 200", async () => {

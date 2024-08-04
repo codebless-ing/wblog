d77d7ebccddda2ac3d3f0f4732107219c6a19258
Mts <notcolondee@gmail.com>
2024-08-04 16:50:30 -0300

    Render error pages for captured 500 and 404 HTTPExceptions

--- src/common/middlewares/errorHandler.js
+++ src/common/middlewares/errorHandler.js
@@ -1,32 +1,38 @@
 import { newLogger } from "@common/utils/logger.js";
 import { HttpException } from "@common/exceptions/appExceptions.js";
 
-const logger = newLogger("Middleware|errorHandler")
+const logger = newLogger("Middleware|errorHandler");
 
 function logErrors(err, req, res, next) {
-  if (!(err instanceof HttpException) || err.statusCode != 404) {
-    logger.error(err.stack)
-  }
+    // Doesn't log 404 HTTPErrors
+    if (!(err instanceof HttpException && err.statusCode == 404)) {
+        logger.error("%s", err);
+    }
 
-  next(err)
+    next(err);
 }
 
-function notFoundHandler(req, res, next) {
-  const error = new HttpException(404, "Not found!");
-  next(error);
+function errorHandler(err, req, res, next) {
+    /*
+     * HTTP ERRORS
+     */
+    if (err instanceof HttpException) {
+        switch (err.statusCode) {
+            case 404:
+                return next();
+            case 500:
+                return res.status(500).render("errors/500", { message: err.httpMessage, reason: err.httpReason });
+
+            default:
+                break;
+        }
+    }
+
+    return res.status(500).render("errors/500");
 }
 
-function xhrErrorHandler(err, req, res, next) {
-  if (req.xhr) {
-    res.status(500).send({ error: 'Something failed!' })
-  } else {
-    next(err)
-  }
+function notFoundHandler(req, res) {
+    return res.status(404).render("errors/404");
 }
 
-function errorHandler(err, req, res) {
-  res.status(err.statusCode ?? 500)
-  res.send(err.httpMessage ?? "Internal Server Error")
-}
-
-export { logErrors, xhrErrorHandler, errorHandler, notFoundHandler }
+export { logErrors, errorHandler, notFoundHandler };

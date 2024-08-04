46862d999058fbf4695932d25b033bd5fc8c7b0c
Mts <notcolondee@gmail.com>
2024-08-04 16:48:33 -0300

    Change order of middlewares execution

--- src/app.js
+++ src/app.js
@@ -3,38 +3,32 @@ import "express-async-errors";
 
 import config from "./config.js";
 import { connectToDatabase } from "@models/index.js";
-import { errorHandler, xhrErrorHandler, logErrors, notFoundHandler } from "@middlewares/errorHandler.js";
+import { errorHandler, logErrors, notFoundHandler } from "@middlewares/errorHandler.js";
 import { consoleLogger } from "@common/utils/logger.js";
 
-import routes from '@routes/index.js';
+import routes from "@routes/index.js";
 
 /* Model */
-await connectToDatabase(
-  config.db.uri,
-  config.db.user,
-  config.db.pass,
-  config.db.database
-)
+await connectToDatabase(config.db.uri, config.db.user, config.db.pass, config.db.database);
 
 /* HTTP */
 const app = express();
 
-app.set('view engine', 'pug');
-app.set('views', './src/resources/views'); 
+app.set("view engine", "pug");
+app.set("views", "./src/resources/views");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/', routes);
+app.use("/", routes);
 
 /** Error handling */
+app.use(logErrors);
+app.use(errorHandler);
 app.use(notFoundHandler);
-app.use(logErrors)
-app.use(xhrErrorHandler)
-app.use(errorHandler)
 
 // Spin up the HTTP server
 app.listen(config.app.port, async () => {
-  consoleLogger.info(`App listening on port ${config.app.port}`);
-})
+    consoleLogger.info(`App listening on port ${config.app.port}`);
+});
 
 export default app;

330827714e0adfe9bb9dc6fbe46d8c315ae905ae
Mts <notcolondee@gmail.com>
2024-10-20 22:06:53 -0300

    Code cleanup

--- src/app/repositories/article.repository.js
+++ src/app/repositories/article.repository.js
@@ -4,12 +4,12 @@ class ArticleRepository {
     async filter(text, tags) {
         const article = await new Article();
 
-        // Search for text in the post title (case insensitive)
+        // Search for text in the article title (case insensitive)
         const query = {
             title: { $regex: String(text), $options: "i" },
         };
 
-        // Optionally, the post must ALSO contain one of the given tags
+        // Optionally, the article must ALSO contain one of the given tags
         if (tags.length) {
             query.$or = [];
 

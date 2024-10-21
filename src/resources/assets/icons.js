406447c2e6ffcb0f85532b6ef8917da7fb0c44fd
Chrystian <chrystian_alex@hotmail.com>
2024-10-21 19:29:21 -0300

    Change filter style

--- src/resources/assets/icons.js
+++ src/resources/assets/icons.js
@@ -1,9 +1,10 @@
 /**
+ * Icon list: https://pictogrammers.com/libraries/
  * List of icons. Key is icon set prefix, value is array of icons
  *
  * @type {Record<string, string[]>}
  */
 export default {
-    mdi: ["logout", "user"],
+    mdi: ["logout", "user", "magnify"],
     "mdi-light": ["alert-circle"],
 };

a472c7ef9dfeb9c60d6cc23de581c66040e9c9b9
Mts <notcolondee@gmail.com>
2024-10-22 01:08:19 -0300

    Limit the body length of each article in ListArticleInputDto

--- src/common/dto/article/list.dto.js
+++ src/common/dto/article/list.dto.js
@@ -1,6 +1,11 @@
 import Joi from "joi";
 import { BaseOutputDto } from "../dto.js";
 
+const bodyLimit = 512;
+function truncate(content) {
+    return content.substring(0, bodyLimit - 3) + "...";
+}
+
 class ListArticleInputDto {
     static SCHEMA = Joi.object({
         title: Joi.string().allow(""),
@@ -31,7 +36,7 @@ class ListArticleOutputDto extends BaseOutputDto {
             this.data[k] = {
                 _id: articles[k]._id,
                 title: articles[k].title,
-                body: articles[k].body,
+                body: articles[k].body.length > bodyLimit ? truncate(articles[k].body) : articles[k].body,
                 tags: articles[k].tags,
             };
         }

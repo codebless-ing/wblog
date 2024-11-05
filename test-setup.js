048466c8d154666d54da567dc03aa5a7f8ac6f5d
Mts <notcolondee@gmail.com>
2024-11-05 14:49:25 -0300

    Automatically give a value to the SESSION_SECRET env var before running the tests

--- test-setup.js
+++ test-setup.js
@@ -1,3 +1,12 @@
 import { jest } from '@jest/globals';
+import * as crypto from "node:crypto";
 
 global.jest = jest;
+
+// Generate a new HTTP secret in case it's not defined in the .env file
+Object.defineProperty(process.env, "SESSION_SECRET", {
+    value: process.env.SESSION_SECRET || crypto.randomBytes(20).toString("hex"),
+    writable: false,
+    enumerable: true,
+    configurable: true,
+});

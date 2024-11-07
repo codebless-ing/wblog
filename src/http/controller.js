40cf9dc532a92a6e7cca8767bdfe95a9e23c7266
Mts <notcolondee@gmail.com>
2024-11-06 22:21:34 -0300

    Create mechanism for passing messages through different layers and between sessions (#40)
    * Install express-flash for flash messages
    
    * Use the express-flash middleware
    
    * Merge session and cookie into one property
    
    * Update how reportBadData is called
    
    * Make reportBadData save the validation errors into the flash messages list
    
    * Add a div for validation messages
    
    * Remove unused variable
    
    * Update comments

--- src/http/controller.js
+++ src/http/controller.js
@@ -7,13 +7,38 @@ const logger = newLogger("Controller|BaseController");
 import { HttpException } from "@common/exceptions/appExceptions.js";
 
 class BaseController {
-    reportBadData(error, data) {
+    reportBadData(error, req, res) {
         if (!(error instanceof ValidationError)) {
             throw error;
         }
 
-        logger.info("Invalid data (%j): %s", data, error);
-        throw new HttpException(400, error.message);
+        // Softly check if res is a response object
+        if (typeof res == "undefined" || typeof res.app == "undefined" || typeof res.locals == "undefined") {
+            throw new HttpException(500, "Response is required for reporting a Validation Error");
+        }
+
+        // Softly check if req is a request object
+        if (typeof req == "undefined" || typeof req.app == "undefined" || typeof req.method == "undefined") {
+            throw new HttpException(500, "Request is required for reporting a Validation Error");
+        }
+
+        // Copy the validation errors, if any, to the list of flash messages
+        if (Array.isArray(error.details)) {
+            const validation = [];
+
+            for (const d of error.details) {
+                validation.push({
+                    message: d.message,
+                    layer: "validation",
+                    type: "info"
+                })
+            }
+
+            req.flash('validation', validation);
+        }
+
+        logger.info("Invalid data (%j): %s", req.body, error);
+        return res.status(400);
     }
 }
 

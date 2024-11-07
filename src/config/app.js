1a8a41bb1b4c04466ab397f8ff928089b05a4681
Mts <notcolondee@gmail.com>
2024-11-06 21:32:48 -0300

    Merge session and cookie into one property

--- src/config/app.js
+++ src/config/app.js
@@ -6,13 +6,16 @@ const app = {
     port: env.PORT ?? 6010,
     name: env.ENV_NAME ? `wlog:${env.ENV_NAME}` : "wlog",
     http: {
-        cookie: {
-            httpOnly: true, // By design, our client won't rely on Javascript
-            sameSite: true,
-            secure: env.NODE_ENV == "production" // HTTPS only enabled for prod environment
-        },
         session: {
-            secret: env.SESSION_SECRET
+            secret: env.SESSION_SECRET,
+            unset: "destroy",
+            resave: false,
+            saveUninitialized: true,
+            cookie: {
+                httpOnly: true, // By design, our client won't rely on Javascript
+                sameSite: true,
+                secure: env.NODE_ENV == "production" // HTTPS only enabled for prod environment
+            }
         }
     },
 };

3943450f4db5ebda6b3eabc728265e2ca9727bc4
Mts <notcolondee@gmail.com>
2025-01-21 23:45:40 -0300

    Add timezone and timestamps to the article schema

--- src/app/models/article.model.js
+++ src/app/models/article.model.js
@@ -2,39 +2,42 @@ import BaseModel from "./model.js";
 import { Schema } from "mongoose";
 
 class Article extends BaseModel {
-    static SCHEMA = new Schema({
-        title: {
-            type: String,
-            minLength: [3, "Title must have at least 3 characters"],
-            maxLength: 250,
-            required: [true, "Title is a required field"],
-        },
-        body: {
-            type: String,
-            maxLength: 500000,
-            required: [true, "Body is a required field"],
-        },
-        tags: {
-            type: [String],
-            validate: [
-                (value) => {
-                    for (let k in value) {
-                        if (value[k].length < 2) {
-                            return false;
+    static SCHEMA = new Schema(
+        {
+            title: {
+                type: String,
+                minLength: [3, "Title must have at least 3 characters"],
+                maxLength: 250,
+                required: [true, "Title is a required field"],
+            },
+            body: {
+                type: String,
+                maxLength: 500000,
+                required: [true, "Body is a required field"],
+            },
+            tags: {
+                type: [String],
+                validate: [
+                    (value) => {
+                        for (let k in value) {
+                            if (value[k].length < 2) {
+                                return false;
+                            }
                         }
-                    }
-                    return value.length <= 10;
-                },
-                "You can only have a max of 10 tags and each of them must have at least 2 characters",
-            ],
+                        return value.length <= 10;
+                    },
+                    "You can only have a max of 10 tags and each of them must have at least 2 characters",
+                ],
+            },
+            user_id: { type: Schema.Types.ObjectId, ref: "User" },
+            timezone: {
+                type: String, // TZ Identifier
+                enum: Intl.supportedValuesOf("timeZone"),
+                default: "UTC",
+            },
         },
-        user_id: String,
-        timezone: {
-            type: String,
-            min: 4,
-            max: 5,
-        },
-    });
+        { timestamps: true }
+    );
 }
 
 export default Article;
